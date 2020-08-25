eab3d5c4cf760c7cfde76d22c0862889a9203b68
Developer <earlg@dnamicro.com>
2020-08-25 12:59:01 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Home from "./components/Home";
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 const App = () => {
   return (
     <div>
-      <Home />
+        <Home />
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,46 @@ import React from "react";
 import Product from "./products/Product";
 import Category from "./categories/Category";
 import Order from "./orders/Order";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <p>
-          <Product />
-          <Category />
-          <Order />
-      </p>
-    </div>
-  );
-};
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+          </ul>
+        </nav>
 
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/products">
+            <Product />
+          </Route>
+          <Route path="/categories">
+            <Category />
+          </Route>
+          <Route path="/orders">
+            <Order />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 export default Home;
